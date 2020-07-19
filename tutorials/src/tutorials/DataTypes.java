f66433bbe5927aaa927dbea583b89c9b09fe20f1
mcfudd <christian.mans@gmail.com>
2020-07-20 10:29:57 +1200

    initial upload

---
+++ tutorials/src/tutorials/DataTypes.java
@@ -0,0 +1,18 @@
+package tutorials;
+
+public class DataTypes {
+
+	public static void main(String[] args)
+	{
+		byte myByte = 5;				// byte
+		short myShort = 5000;			// short
+		int myInt = 65000;				// integer
+		long myLong = 1000000;			// long
+		float myFloat = 10.034f;		// float
+		double myDouble = 3.14159;		// double
+		boolean myBoolean = true;		// boolean
+		char myChar = 'm';				// char
+		
+	}
+
+}
